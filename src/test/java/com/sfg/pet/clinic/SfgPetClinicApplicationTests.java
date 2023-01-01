60de8804604cb3f3d8b9a5c2534b02cdafc4bb60
Sofimedina <sofi.k.medina@gmail.com>
2022-12-31 21:41:26 -0300

    first commit
    Signed-off-by: Sofimedina <sofi.k.medina@gmail.com>

---
+++ src/test/java/com/sfg/pet/clinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.pet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
