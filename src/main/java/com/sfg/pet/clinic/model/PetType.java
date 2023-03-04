886f8bdf2acc6188ed25b115c6089e99bae6b2ee
Karina Sofia Medina <sofi.k.medina@gmail.com>
2023-03-04 00:32:21 -0300

    add-pojos
    Signed-off-by: Karina Sofia Medina <sofi.k.medina@gmail.com>

---
+++ src/main/java/com/sfg/pet/clinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.sfg.pet.clinic.model;
+
+public class PetType {
+    private String name;
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
