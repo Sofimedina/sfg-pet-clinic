1ccb43be663dc5f67421c9028e21f8b5fbcbf33a
Karina Sofia Medina <kamedina@expediagroup.com>
2023-05-05 00:11:42 -0300

    solving merge problems
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

--- pet-clinic-data/src/main/java/com/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/services/OwnerService.java
@@ -4,6 +4,13 @@ import com.model.Owner;
 
 import java.util.Set;
 
+<<<<<<< Updated upstream
 public interface OwnerService extends CrudService<Owner,Long> {
+=======
+public interface OwnerService  {
+    Owner getById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+>>>>>>> Stashed changes
     Set<Owner> findByLastName(String lastName);
 }
--- pet-clinic-data/src/main/java/com/services/VetService.java
+++ pet-clinic-data/src/main/java/com/services/VetService.java
@@ -2,5 +2,16 @@ package com.services;
 
 import com.model.Vet;
 
+<<<<<<< Updated upstream
 public interface VetService extends CrudService<Vet,Long> {
+=======
+import java.util.Set;
+
+public interface VetService {
+
+
+    Vet getById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+>>>>>>> Stashed changes
 }
