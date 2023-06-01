4b3cec77e3cf763c078b664adb0cbe5264830fa6
Karina Sofia Medina <kamedina@expediagroup.com>
2023-06-01 19:36:45 -0300

    adding vets and owners view controllers
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

---
+++ pet-clinic-web/src/main/java/sofi/com/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package sofi.com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owner","owner/index"})
+    public String owner(){
+        return "owner/index";
+    }
+}
